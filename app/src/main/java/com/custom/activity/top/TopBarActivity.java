be9d03904415eb16f3e296bc61bc8db556f1ab0c
Kevin Gu <gubaijing2004@163.com>
2016-05-11 18:34:52 +0800

    update

---
+++ app/src/main/java/com/custom/activity/top/TopBarActivity.java
@@ -0,0 +1,15 @@
+package com.custom.activity.top;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.custom.R;
+
+public class TopBarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_bar);
+    }
+}
