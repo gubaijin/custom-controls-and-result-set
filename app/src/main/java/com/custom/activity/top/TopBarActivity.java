ab48cdcf8f8b90ee045a2ce33460a9f454b8b42b
Kevin Gu <gubaijing2004@163.com>
2016-05-12 17:45:17 +0800

    完成自定义topBar

--- app/src/main/java/com/custom/activity/top/TopBarActivity.java
+++ app/src/main/java/com/custom/activity/top/TopBarActivity.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.custom.R;
+import com.custom.controls.top.TopBar;
 
 public class TopBarActivity extends AppCompatActivity {
 
+    private TopBar topBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_bar);
+        topBar = (TopBar) findViewById(R.id.topbar);
+        topBar.setCenterText("代码中设置的title");
     }
 }
--- app/src/main/java/com/custom/controls/top/TopBar.java
+++ app/src/main/java/com/custom/controls/top/TopBar.java
@@ -1,15 +1,19 @@
-package com.custom.top;
+package com.custom.controls.top;
 
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.custom.R;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by Kevin on 2016/5/11.
@@ -34,9 +38,19 @@ public class TopBar extends RelativeLayout {
     private boolean rightImgIsShow;//右侧图标是否显示
     private Drawable rightImg;
 
+    //左右图标
     private ImageView img_left, img_right;
+    //左中右文字
     private TextView txt_left, txt_center, txt_right;
     private LayoutParams leftImgParams, leftTxtParams, centerParams, rightTxtParams, rightImgParams;
+
+    public TopBar(Context context) {
+        super(context);
+    }
+
+    public TopBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
 
     public TopBar(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -47,9 +61,18 @@ public class TopBar extends RelativeLayout {
             leftImg = array.getDrawable(R.styleable.topBar_leftImg);
             img_left = new ImageView(context);
             img_left.setImageDrawable(leftImg);
+            img_left.setId(generateViewId());
             leftImgParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
             leftImgParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+            leftImgParams.addRule(RelativeLayout.CENTER_VERTICAL);
             addView(img_left, leftImgParams);
+
+            img_left.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    leftClick();
+                }
+            });
         }
 
         leftText = array.getString(R.styleable.topBar_leftText);
@@ -68,15 +91,25 @@ public class TopBar extends RelativeLayout {
         if (rightImgIsShow) {
             rightImg = array.getDrawable(R.styleable.topBar_rightImg);
             img_right = new ImageView(context);
+            img_right.setId(generateViewId());
             img_right.setImageDrawable(rightImg);
             rightImgParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
             rightImgParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+            rightImgParams.addRule(RelativeLayout.CENTER_VERTICAL);
             addView(img_right, rightImgParams);
+
+            img_right.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    rightClick();
+                }
+            });
         }
         array.recycle();
 
         txt_left = new TextView(context);
         txt_left.setText(leftText);
+        txt_left.setId(generateViewId());
         txt_left.setTextSize(leftTextSize);
         txt_left.setTextColor(leftTextColor);
 
@@ -85,20 +118,24 @@ public class TopBar extends RelativeLayout {
             leftTxtParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);    //addRule一个参数
         } else {
             leftTxtParams.addRule(RelativeLayout.RIGHT_OF, img_left.getId()); //addRule一个参数
+            leftTxtParams.addRule(RelativeLayout.END_OF, img_left.getId());
         }
+        leftTxtParams.addRule(RelativeLayout.CENTER_VERTICAL);
         addView(txt_left, leftTxtParams);
 
         txt_center = new TextView(context);
         txt_center.setText(centerText);
+        txt_center.setId(generateViewId());
         txt_center.setTextSize(centerTextSize);
         txt_center.setTextColor(centerTextColor);
 
         centerParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-        centerParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        centerParams.addRule(RelativeLayout.CENTER_IN_PARENT);
         addView(txt_center, centerParams);
 
         txt_right = new TextView(context);
         txt_right.setText(rightText);
+        txt_right.setId(generateViewId());
         txt_right.setTextSize(rightTextSize);
         txt_right.setTextColor(rightTextColor);
 
@@ -107,7 +144,65 @@ public class TopBar extends RelativeLayout {
             rightTxtParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);    //addRule一个参数
         } else {
             rightTxtParams.addRule(RelativeLayout.LEFT_OF, img_right.getId()); //addRule一个参数
+            rightTxtParams.addRule(RelativeLayout.START_OF, img_right.getId());
         }
+        rightTxtParams.addRule(RelativeLayout.CENTER_VERTICAL);
         addView(txt_right, rightTxtParams);
+
+        txt_left.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                leftClick();
+            }
+        });
+
+        txt_center.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                centerClick();
+            }
+        });
+        txt_right.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rightClick();
+            }
+        });
+    }
+
+    private static final AtomicInteger sNextGeneratedId = new AtomicInteger(1);
+
+    public static int generateViewId() {
+        for (; ; ) {
+            final int result = sNextGeneratedId.get();
+            // aapt-generated IDs have the high byte nonzero; clamp to the range under that.
+            int newValue = result + 1;
+            if (newValue > 0x00FFFFFF) newValue = 1; // Roll over to 1, not 0.
+            if (sNextGeneratedId.compareAndSet(result, newValue)) {
+                return result;
+            }
+        }
+    }
+
+    //左侧点击，根据实际需求修改
+    public void leftClick() {
+        Toast.makeText(getContext(), "左侧被点击", Toast.LENGTH_SHORT).show();
+    }
+
+    //中间点击，根据实际需求修改
+    public void centerClick() {
+        Toast.makeText(getContext(), "中间被点击", Toast.LENGTH_SHORT).show();
+    }
+
+    //右侧点击，根据实际需求修改
+    public void rightClick() {
+        Toast.makeText(getContext(), "右侧被点击", Toast.LENGTH_SHORT).show();
+    }
+
+    //对外暴露可以设置标题的方法
+    public void setCenterText(String centerText) {
+        if(null != txt_center){
+            txt_center.setText(centerText);
+        }
     }
 }
